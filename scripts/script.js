e1bccc0790c3c799d4e87d38e0f314d570c2fb13
Rafael Cerqueira <rafaelcerqueiraf@outlook.com>
2024-07-20 18:38:24 -0300

    funcionalidades criptografar e descriptografar adicionadas

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,42 @@ const today = new Date();
 const currentYear = today.getFullYear();
 document.getElementById('currentYear').textContent = currentYear;
 
-const btnCodificar = document.getElementById('btn-codificar');
-const btnDecodificar = document.getElementById('btn-decodificar');
-const btnCopiar = document.getElementById('btn-copiar'); 
+const btnCriptografar = document.getElementById('btn-codificar');
+const btnDescriptografar = document.getElementById('btn-decodificar');
+const btnCopiar = document.getElementById('btn-copiar');
+const textArea = document.getElementById('texto_principal');
 
+function criptografar(texto) {
+    const substituicoes = {
+        'e': 'enter',
+        'i': 'imes',
+        'a': 'ai',
+        'o': 'ober',
+        'u': 'ufat'
+    }
 
-btnCodificar.addEventListener('click', () => {
+    return texto.split('').map(letra => substituicoes[letra] || letra).join('');
+}
+
+function descriptografar(texto) {
+    const substituicoesInversas = {
+        'enter': 'e',
+        'imes': 'i',
+        'ai': 'a',
+        'ober': 'o',
+        'ufat': 'u'
+    };
+
+    const substituicoesOrdenadas = Object.keys(substituicoesInversas).sort((a, b) => b.length - a.length);
+
+    substituicoesOrdenadas.forEach(substituicao => {
+        texto = texto.split(substituicao).join(substituicoesInversas[substituicao]);
+    });
+
+    return texto;
+};
+
+btnCriptografar.addEventListener('click', () => {
     const texto = document.getElementById('texto_principal').value;
     const messageArea = document.querySelector('.message_area');
     const mainContainerMessage = document.querySelector('.main__container__message');
@@ -16,16 +46,41 @@ btnCodificar.addEventListener('click', () => {
     const p = document.createElement('p');
     p.id = 'mensagem_codificada';
     p.className = 'mensagem_codificada';
-    p.textContent = texto;
-
+    p.textContent = criptografar(texto);
+    
     // substitui o conteúdo da div message_area pelo novo p
     messageArea.innerHTML = '';
     messageArea.appendChild(p);
 
     btnCopiar.style.visibility = 'visible';
     mainContainerMessage.style.display = 'flex';
-    mainContainerMessage.style.justifyContent = 'space-between';    
-    console.log('Texto codificado');
+    mainContainerMessage.style.justifyContent = 'space-between';
+    p.style.fontSize = '1.5rem';
+    
+});
+
+
+btnDescriptografar.addEventListener('click', () => {
+    const texto = document.getElementById('texto_principal').value;
+    const messageArea = document.querySelector('.message_area');
+    const mainContainerMessage = document.querySelector('.main__container__message');
+
+    //cria um novo elemento p
+    const p = document.createElement('p');
+    p.id = 'mensagem_codificada';
+    p.className = 'mensagem_codificada';
+    p.textContent = descriptografar(texto);
+  
+    
+    // substitui o conteúdo da div message_area pelo novo p
+    messageArea.innerHTML = '';
+    messageArea.appendChild(p);
+
+    btnCopiar.style.visibility = 'visible';
+    mainContainerMessage.style.display = 'flex';
+    mainContainerMessage.style.justifyContent = 'space-between';
+    p.style.fontSize = '1.5rem';
+
 });
 
 btnCopiar.addEventListener('click', () => {
@@ -47,3 +102,4 @@ btnCopiar.addEventListener('click', () => {
     }, 1000);
 
 });
+
